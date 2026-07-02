e9a85f6b5bf2eee0f633dcce39596ff86cda0861
Teejuta Sriwaranon <teejuta.sriwaranon@gmail.com>
2026-07-03 01:19:02 +0700

    feat: visualize_cv()

--- src/visualizer.py
+++ src/visualizer.py
@@ -2,11 +2,13 @@ import matplotlib.pyplot as plt
 import math
 import mediapipe as mp
 
+import cv2
+
 
 class Visualizer:
 
     @staticmethod
-    def visualize(image, pose_landmarker_result):
+    def visualize_image(image, pose_landmarker_result):
         h, w, _ = image.shape
 
         _, ax = plt.subplots()
@@ -37,3 +39,33 @@ class Visualizer:
                 ax.plot([x_start, x_end], [y_start, y_end], c="blue", linewidth=1)
 
         plt.show()
+
+    @staticmethod
+    def visualize_cv(image, pose_landmarker_result):
+        h, w, _ = image.shape
+
+        for pose in pose_landmarker_result.pose_landmarks:
+            for landmark in pose:
+                x = w - math.ceil(landmark.x * w)
+                y = math.ceil(landmark.y * h)
+
+                cv2.circle(image, (x, y), 5, (0, 0, 255), -1)
+
+            connections = mp.tasks.vision.PoseLandmarksConnections.POSE_LANDMARKS
+
+            for connection in connections:
+                start_idx = connection.start
+                end_idx = connection.end
+
+                start_landmark = pose[start_idx]
+                end_landmark = pose[end_idx]
+
+                x_start = w - int(start_landmark.x * w)
+                y_start = int(start_landmark.y * h)
+
+                x_end = w - int(end_landmark.x * w)
+                y_end = int(end_landmark.y * h)
+
+                cv2.line(image, (x_start, y_start), (x_end, y_end), (255, 0, 0), 2)
+
+        return image
